f2513dac3c738f51274391d4b6345004c89d4b31
joflorid <joflorid@student.42urduliz.com>
2025-11-24 17:04:50 +0100

    Adding Jupyter Notebook python overview & Adding ex2 from python picine & Adding main.py to test the exercises

--- python_piscine/ex2/ft_harvest_total.py
+++ python_piscine/ex2/ft_harvest_total.py
@@ -1,28 +1,21 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    ft_hello_garden.py                                 :+:      :+:    :+:    #
+#    ft_harvest_total.py                                :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: joflorid <joflorid@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/11/24 16:50:17 by joflorid          #+#    #+#              #
-#    Updated: 2025/11/24 16:50:18 by joflorid         ###   ########.fr        #
+#    Created: 2025/11/24 16:49:47 by joflorid          #+#    #+#              #
+#    Updated: 2025/11/24 16:58:44 by joflorid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-def ft_hello_garden() -> None:
-    """
-    DEFINITION
-        ft_hello_garden() prints a message in the CLI
-
-    PARAMETERS
-        Nothing
-
-    RETURN
-        Nothing
-    """
-    print("Hello, Garden Community!")
+def ft_harvest_total() -> None:
+    day_1 = int(input("Day 1 harvest: "))
+    day_2 = int(input("Day 2 harvest: "))
+    day_3 = int(input("Day 3 harvest: "))
+    print(f"Total harvest: {day_1 + day_2 + day_3}")
 
 
 # if __name__ == '__main__':
-# 	ft_hello_garden()
+#     ft_harvest_total()
