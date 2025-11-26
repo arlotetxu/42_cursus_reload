8ee87f547ef255bdaac1ad0973d691bbe8062932
joflorid <joflorid@student.42urduliz.com>
2025-11-26 15:56:48 +0100

    Deleting the docstring from ft_plot_area y adding debuging config file for python

--- python_piscine/ft_plot_area.py
+++ python_piscine/ft_plot_area.py
@@ -6,22 +6,11 @@
 #    By: joflorid <joflorid@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/11/24 16:50:08 by joflorid          #+#    #+#              #
-#    Updated: 2025/11/24 16:50:09 by joflorid         ###   ########.fr        #
+#    Updated: 2025/11/26 13:42:45 by joflorid         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 def ft_plot_area() -> None:
-    """
-    DEFINITION
-        ft_plot_area() requests from the user the length and width of a garden
-        and returns the area.
-
-    PARAMETERS
-        Nothing
-
-    RETURN
-        The area of the garden calculate as length * width.
-    """
     length = int(input("Enter length: "), base=10)
     width = int(input("Enter width: "), base=10)
     print(f"Plot area: {length * width}")
