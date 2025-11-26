b5489cd84307ad2e4de7a6518af3134aa90c5bd3
arlotetxu <arlotetxu@protonmail.com>
2025-11-26 19:03:19 +0100

    Adding last 2 function ft_garden_summary & ft_seed_inventory of the module 1 python piscine

---
+++ python_piscine/ft_garden_summary.py
@@ -0,0 +1,11 @@
+def ft_garden_summary() -> None:
+    g_name = input("Enter garden name: ")
+    plants = int(input("Enter number of plants: "))
+    status = "Growing well!"
+    print(f"Garden: {g_name}\n"
+          f"Plants: {plants}\n"
+          f"Status: {status}")
+
+
+# if __name__ == '__main__':
+#     ft_garden_summary()
---
+++ python_piscine/ft_seed_inventory.py
@@ -0,0 +1,20 @@
+def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
+    str_final = ""
+    if unit == "packets":
+        str_final = (f"{seed_type.capitalize()} seeds: {quantity} /"
+                     f"packets available")
+    elif unit == "grams":
+        str_final = f"{seed_type.capitalize()} seeds: {quantity} grams total"
+    elif unit == "area":
+        str_final = (f"{seed_type.capitalize()} seeds: covers {quantity} /"
+                     f"square meters")
+    else:
+        str_final = (f"{seed_type.capitalize()} seeds: {quantity} /"
+                     f"Unknown unit type")
+    print(str_final)
+
+
+# if __name__ == "__main__":
+#     ft_seed_inventory("tomato", 15, "packets")
+#     ft_seed_inventory("carrot", 8, "grams")
+#     ft_seed_inventory("lettuce", 12, "area")
