fad14f508b425c89b92743bc04ee1808f9cd3091
joflorid <joflorid@student.42urduliz.com>
2026-03-10 17:50:26 +0100

    Going on with parsing process. WIP

--- fly-in/src/parsing/map_parsing.py
+++ fly-in/src/parsing/map_parsing.py
@@ -1,6 +1,7 @@
 import sys
 from typing import List, Dict
 from src.conf.enums import Colors
+from src.parsing.data_model_connects import DataValidator
 from icecream import ic
 
 def read_map(map: str) -> List[str]:
@@ -43,12 +44,19 @@ def get_map_info(map_lines: List[str]) -> Dict[str: List | int]:
             continue
 
     map_info["hubs"] = hubs
-    map_info["connect"] = conections
-    ic(map_info)
+    map_info["connects"] = conections
+    # ic(map_info)
     return map_info
 
-def parse_map(map: str) -> None:
+def data_validation(map: str):
+    map_data = {}
     map_lines = read_map(map)
     map_info = get_map_info(map_lines)
+    connects_data = DataValidator(map_connects=map_info.get("connects"))
+    map_data["connections"] = connects_data
+    ic(map_data.get("connections").map_connects[0])
 
 
+
+def parse_map(map: str) -> None:
+    map_data = data_validation(map)
