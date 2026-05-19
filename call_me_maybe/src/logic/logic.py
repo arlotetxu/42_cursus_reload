1580f0bff36a5cd41b02aa94573dff53a6fde3d1
joflorid <joflorid@student.42urduliz.com>
2026-05-19 12:53:37 +0200

    Adapting an issue with mypy in Campus computer. Removing not needed import modules

--- call_me_maybe/src/logic/logic.py
+++ call_me_maybe/src/logic/logic.py
@@ -1,17 +1,17 @@
-from llm_sdk import Small_LLM_Model
-from src.enums.enums import Colors
-from src.validator.path_validator import PathValidator
-from src.validator.output_json_validator import OutputVal
-from src.prompt.prompt import Prompt
-from src.masks.masks import add_name_mask
-from src.logic.print_result import print_result
-from src.logic.param_adaptor import str_param_adaptor
-from pydantic import ValidationError
-from typing import List, Dict, Any
-import numpy as np
 import json
 from pathlib import Path
+from typing import Any, Dict, List
+import numpy as np
+from pydantic import ValidationError
+from llm_sdk import Small_LLM_Model
+from src.enums.enums import Colors
 from src.logic.get_params import get_number_params, get_str_params
+from src.logic.param_adaptor import str_param_adaptor
+from src.logic.print_result import print_result
+from src.masks.masks import add_name_mask
+from src.prompt.prompt import Prompt
+from src.validator.output_json_validator import InfoVal, OutputVal
+from src.validator.path_validator import PathValidator
 
 
 def get_func_name(
@@ -45,10 +45,7 @@ def get_func_name(
             prompt_logits_np = np.array(prompt_logits)
 
             masked_logits_np = add_name_mask(
-                selected_func_name,
-                func_names,
-                vocab_dict,
-                prompt_logits_np
+                selected_func_name, func_names, vocab_dict, prompt_logits_np
             )
             next_token_id = int(np.argmax(masked_logits_np))
             next_token = llm.decode([next_token_id])
@@ -87,16 +84,18 @@ def get_func_parameters(
     """
 
     func_params = [
-        func_obj.parameters for func_obj in full_func_def
-        if func_obj.name == selected_func_name]
+        func_obj.parameters
+        for func_obj in full_func_def
+        if func_obj.name == selected_func_name
+    ]
 
     parameters_dict: Dict[str, str | float] = {}
 
-    prompt += "\"parameters\": {"
+    prompt += '"parameters": {'
     try:
         for param in func_params:
             for param_name, param_type in param.items():
-                prompt += f"\"{param_name}\": "
+                prompt += f'"{param_name}": '
                 if param_type.type in ["number", "integer"]:
                     prompt, num_parameter = get_number_params(
                         llm, prompt, func_params, vocab_dict, param_type.type
@@ -104,7 +103,7 @@ def get_func_parameters(
                     parameters_dict[param_name] = num_parameter
 
                 if param_type.type == "string":
-                    prompt += "\""
+                    prompt += '"'
                     prompt, str_parameter = get_str_params(
                         llm, prompt, func_params, vocab_dict
                     )
@@ -118,8 +117,8 @@ def get_func_parameters(
 
 
 def create_output_file(
-    path2jsons: PathValidator,
-    output_info: List[Dict[str, Any]]
+        path2jsons: PathValidator,
+        output_info: List[InfoVal]
 ) -> int:
     """
     Validates and writes the LLM output to a JSON file.
@@ -137,20 +136,24 @@ def create_output_file(
         out_validator = OutputVal(items=output_info)
         validated_data = out_validator.model_dump()
         output_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(output_path, mode='x') as fd:
+        with open(output_path, mode="x") as fd:
             json.dump(validated_data.get("items", ""), fd, indent=4)
     except (ValidationError, TypeError):
-        print(f"{Colors.RED.value}[ERROR] - "
-              f"The output file ({output_path}) has a wrong structure "
-              f"or your are trying to create a JSOn file from a wrong source. "
-              "Exiting..."
-              f"{Colors.RESET.value}")
+        print(
+            f"{Colors.RED.value}[ERROR] - "
+            f"The output file ({output_path}) has a wrong structure "
+            f"or your are trying to create a JSOn file from a wrong source. "
+            "Exiting..."
+            f"{Colors.RESET.value}"
+        )
         return 8
     except PermissionError:
-        print(f"{Colors.RED.value}[ERROR] - "
-              f"The output file ({output_path}) couldn't be saved. "
-              "Check the folder path permissions and try it again."
-              f"{Colors.RESET.value}")
+        print(
+            f"{Colors.RED.value}[ERROR] - "
+            f"The output file ({output_path}) couldn't be saved. "
+            "Check the folder path permissions and try it again."
+            f"{Colors.RESET.value}"
+        )
         return 7
     return 0
 
@@ -166,57 +169,66 @@ def get_output_info(path2jsons: PathValidator, init_prompt: str) -> int:
     Returns:
         int: 0 on success, error code otherwise.
     """
-    output_info = []
+    output_info: List[InfoVal] = []
     llm = Small_LLM_Model()
     full_func_def = Prompt(input_paths=path2jsons).get_func_def().functions
     full_func_call = Prompt(input_paths=path2jsons).get_func_call().prompts
     func_names = [func.name for func in full_func_def]
-    func_call = [prompt.get("prompt", None) for prompt in full_func_call]
+    func_call = [prompt.get("prompt", "") for prompt in full_func_call]
 
     vocab_json_path = llm.get_path_to_vocab_file()
     try:
-        with open(vocab_json_path, mode='r') as vj:
+        with open(vocab_json_path, mode="r") as vj:
             vocab_dict = json.load(vj)
     except FileNotFoundError:
-        print(f"{Colors.RED.value}[ERROR] - "
-              "The vocab.json file couldn't be found. "
-              f"Please, check it and try again.{Colors.RESET.value}")
+        print(
+            f"{Colors.RED.value}[ERROR] - "
+            "The vocab.json file couldn't be found. "
+            f"Please, check it and try again.{Colors.RESET.value}"
+        )
         return 4
     except (PermissionError, json.JSONDecodeError):
-        print(f"{Colors.RED.value}[ERROR] - "
-              "The vocab.json file couldn't be open. "
-              "Please, check the file permissions/structure and try it again."
-              f"{Colors.RESET.value}")
+        print(
+            f"{Colors.RED.value}[ERROR] - "
+            "The vocab.json file couldn't be open. "
+            "Please, check the file permissions/structure and try it again."
+            f"{Colors.RESET.value}"
+        )
         return 5
 
     try:
         for i in range(len(func_call)):
             prompt = (init_prompt +
-                      "[\n" + "{\n\t\"prompt\":" + f"\"{func_call[i]}\",\n")
-            prompt += "\"name\": \""
+                      "[\n" + '{\n\t"prompt":' + f'"{func_call[i]}",\n'
+                      )
+            prompt += '"name": "'
 
             selected_func_name = get_func_name(
                 prompt, llm, func_names, vocab_dict
                 )
-            prompt += selected_func_name + "\",\n"
+            prompt += selected_func_name + '",\n'
 
             selected_func_params = get_func_parameters(
                 prompt, llm, full_func_def, selected_func_name, vocab_dict
-                )
+            )
 
             print_result(
                 func_call[i], selected_func_name, selected_func_params
                 )
-            output_info.append(dict(
-                prompt=func_call[i],
-                name=selected_func_name,
-                parameters=selected_func_params
-            ))
+            output_info.append(
+                InfoVal(
+                    prompt=func_call[i],
+                    name=selected_func_name,
+                    parameters=selected_func_params,
+                )
+            )
     except ValueError:
-        print(f"{Colors.RED.value}[ERROR] - "
-              "There was an issue while geeting the output information "
-              "with the LLM. "
-              f"{Colors.RESET.value}")
+        print(
+            f"{Colors.RED.value}[ERROR] - "
+            "There was an issue while geeting the output information "
+            "with the LLM. "
+            f"{Colors.RESET.value}"
+        )
         return 6
     ret = create_output_file(path2jsons, output_info)
     if ret:
